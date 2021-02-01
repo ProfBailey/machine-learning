1969b2ad83c1bad813e55e326a66cea655400380
Brittney E. Bailey <bebailey@amherst.edu>
2021-02-01 10:31:06 -0500

    paper code

---
+++ speiser-project/bimm-tree.R
@@ -0,0 +1,3 @@
+# dis_Speiser_2017__Decision-tree-and-random-forest-longitudinal-binary.pdf
+# BiMM-tree functions
+
---
+++ speiser-project/mcart.R
@@ -0,0 +1,5 @@
+# art_Lin-Luo_2019__Multilevel-CART-binary-outcomes.pdf
+
+# algorithm
+
+# empirical demonstration
